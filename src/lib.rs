649a8f9deef3eb1fd4dc21583f0f43bc49da4bd8
lino <lino_snsalias@outlook.com>
2025-01-04 00:45:50 +0800

    Removed unused mod `empty_`;

--- src/lib.rs
+++ src/lib.rs
@@ -5,15 +5,10 @@
 #[cfg(test)]
 extern crate std;
 
-mod empty_;
 mod peeker_;
 mod reader_;
 mod writer_;
 
-pub use empty_::{
-    DisabledPeekAsync, DisabledReadAsync, DisabledWriteAsync,
-    EmptyBuffIter, EmptyBuffIterError,
-} ;
 pub use peeker_::{TrBuffIterPeek, TrBuffIterTryPeek};
 pub use reader_::{TrBuffIterRead, TrBuffIterTryRead};
 pub use writer_::{TrBuffIterWrite, TrBuffIterTryWrite};
