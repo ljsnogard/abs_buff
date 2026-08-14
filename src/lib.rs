0f3635a8ba150e814c306ece797a583e031a0bfd
lino <lino_snsalias@outlook.com>
2026-08-14 21:48:58 +0800

    Fix misconfig in `Cargo.toml`

--- src/lib.rs
+++ src/lib.rs
@@ -57,7 +57,9 @@ pub mod x_deps {
     pub use abs_cancel;
     pub use anylr;
     pub use gen_mcf_macro;
+
+    pub use funty;
 }
 
-#[cfg(any(test, feature = "std_io"))]
+#[cfg(any(test, feature = "std_read"))]
 pub mod as_std_read;
