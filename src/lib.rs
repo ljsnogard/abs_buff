8ca11da056c1c07af9dc146e8cfa70095a5a2bd1
lino <lino_snsalias@outlook.com>
2026-08-17 00:23:18 +0800

    1. Add `as_std_write`, update `as_std_read`; 2. Tidy code style with `cargo fmt`;

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
-﻿// to enable no hand-written poll
+// to enable no hand-written poll
 #![feature(async_fn_traits)]
 #![feature(impl_trait_in_assoc_type)]
 #![feature(unboxed_closures)]
 #![feature(try_trait_v2)]
-
 #![no_std]
 
 // We always pull in `std` during tests, because it's just easier
@@ -23,19 +22,20 @@ mod reader_;
 mod writer_;
 
 pub use demand_::Demand;
-
 pub use peeker_::{TrBuffPeek, TrBuffTryPeek};
 pub use reader_::{TrBuffRead, TrBuffTryRead};
-pub use writer_::{TrBuffWrite, TrBuffTryWrite};
+pub use writer_::{TrBuffTryWrite, TrBuffWrite};
 
 pub mod x_deps {
-    pub use abs_iter;
     pub use abs_cancel;
+    pub use abs_iter;
     pub use anylr;
-    pub use gen_mcf_macro;
-
     pub use funty;
+    pub use gen_mcf_macro;
 }
 
 #[cfg(any(test, feature = "std_read"))]
 pub mod as_std_read;
+
+#[cfg(any(test, feature = "std_write"))]
+pub mod as_std_write;
